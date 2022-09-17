060eac2911555957c2b99b6111881a94e648979f
baki <badrkhatib@hotmail.com>
2022-09-17 14:29:06 +0200

    some changes but not finished

--- export.c
+++ export.c
@@ -6,28 +6,30 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 18:44:58 by bkhatib           #+#    #+#             */
-/*   Updated: 2022/09/16 17:12:03 by bkhatib          ###   ########.fr       */
+/*   Updated: 2022/09/17 13:32:35 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
-void	ft_export(char **env, char **cmd)
+char	**ft_export(char **env, char **cmd)
 {
 	int	nbr_var;
-	int	status;
+	int	index;
 	char	**new_env;
 	
 	nbr_var = 0;
-	status = 0;
+	index = -1;
 	while(env[nbr_var])
 	{
 		if(strcmp(env[nbr_var], cmd[1]) == 0)
-			status = 1;
+			index = nbr_var;
 		nbr_var++;
 	}
-	if(status == 0)
+	if(index != -1)
 		new_env = malloc_env(env, nbr_var + 1);
 	else
 		new_env = malloc_env(env, nbr_var);
+	new_env = get_export(env, new_env, cmd, index);
+	return (new_env);
 }
